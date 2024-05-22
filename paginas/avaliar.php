57e57c53d1b0695b024de9e2eb8290d5d5ff3e9d
Evelyn Sonza <163865340+evelyncsonza@users.noreply.github.com>
2024-05-21 21:02:09 -0300

    Update avaliar.php

--- paginas/avaliar.php
+++ paginas/avaliar.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['usuario_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Avaliação de Receita</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../css/style_avaliacao.css"/>
+    <link rel="stylesheet" href="../css/style_avaliar.css"/>
 </head>
 <body>
 <h1>Avalie</h1>
