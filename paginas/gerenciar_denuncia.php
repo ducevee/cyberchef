336099eed0f5b97c859d4559a7340e209046d9c1
Mateus <mateusschneiderdelpozzo@gmail.com>
2024-05-27 10:17:42 -0300

    Tabela gerenciar avaliação

--- paginas/gerenciar_denuncia.php
+++ paginas/gerenciar_denuncia.php
@@ -4,52 +4,42 @@ session_start();
 
 // Verificar se é administrador
 if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] != 1) {
-header('Location: login.php');
-exit;
+    header('Location: login.php');
+    exit;
 }
-
 
 // Buscar denúncias de receitas
 $denuncias_receitas = $pdo->query("SELECT Denuncia.*, Receita.titulo AS receita_titulo, usuarios.nome AS denunciante_nome FROM Denuncia JOIN Receita ON Denuncia.fk_id_receita = Receita.id_receita JOIN usuarios ON Denuncia.fk_id_usuario = usuarios.id")->fetchAll(PDO::FETCH_ASSOC);
 
 // Buscar denúncias de avaliações
-$denuncias_avaliacoes = $pdo->query("SELECT Denuncia.*, Avaliacao.id_avaliacao, Receita.titulo AS receita_titulo, usuarios.nome AS denunciante_nome FROM Denuncia JOIN Avaliacao ON Denuncia.fk_Avaliacao_id_avaliacao = Avaliacao.id_avaliacao JOIN Receita ON Avaliacao.fk_receita = Receita.id_receita JOIN usuarios ON Denuncia.fk_id_denunciante = usuarios.id WHERE Denuncia.fk_Avaliacao_id_avaliacao IS NOT NULL")->fetchAll(PDO::FETCH_ASSOC);
+$denuncias_avaliacoes = $pdo->query("SELECT Denuncia.*, Avaliacao.id_avaliacao, Receita.titulo AS receita_titulo, usuarios.nome AS denunciante_nome FROM Denuncia JOIN Avaliacao ON Denuncia.fk_Avaliacao_id_avaliacao = Avaliacao.id_avaliacao JOIN Receita ON Avaliacao.fk_receita = Receita.id_receita JOIN usuarios ON Denuncia.fk_id_usuario = usuarios.id WHERE Denuncia.fk_Avaliacao_id_avaliacao IS NOT NULL")->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Administrativa - Gerenciar Receitas</title>
+    <title>Página Administrativa - Gerenciar Denúncias</title>
     <link rel="stylesheet" href="../css/style_home.css">
 </head>
 <body class="admin-home-page">
     <header>
         <nav class="navHeader">
             <a href="index.html" id="link-logo" title="Página inicial">
-            <img src="../css/img/cyber_chef_logo.png" alt="logo" id="logo">
+                <img src="../css/img/cyber_chef_logo.png" alt="logo" id="logo">
             </a>
-            <div class="search-container">
-            <!-- Omitir funcionalidades de pesquisa se não forem relevantes -->
-            </div>
+            <div class="search-container"></div>
             <ul id="lista">
-                <li>
-                <a class="linksHeader" href="gerenciar_denuncia.php">GERENCIAR DENÚNCIAS</a>
-                </li>
-                <li>
-                <a class="linksHeader" href="home_admin.php">GERENCIAR USUÁRIOS</a>
-                </li>
+                <li><a class="linksHeader" href="gerenciar_denuncia.php">GERENCIAR DENÚNCIAS</a></li>
+                <li><a class="linksHeader" href="home_admin.php">GERENCIAR USUÁRIOS</a></li>
             </ul>
             <div class="user">Bem-vindo, Admin!</div>
             <a href="../processos/logout.php" alt="Sair" title="Sair">
-                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#FFF" version="1.1" id="Capa_1" width="25px" height="25px" viewBox="0 0 492.5 492.5" xml:space="preserve">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#FFF" width="25px" height="25px" viewBox="0 0 492.5 492.5">
                     <g>
-                        <path d="M184.646,0v21.72H99.704v433.358h31.403V53.123h53.539V492.5l208.15-37.422v-61.235V37.5L184.646,0z M222.938,263.129   c-6.997,0-12.67-7.381-12.67-16.486c0-9.104,5.673-16.485,12.67-16.485s12.67,7.381,12.67,16.485   C235.608,255.748,229.935,263.129,222.938,263.129z"/>
+                        <path d="M184.646,0v21.72H99.704v433.358h31.403V53.123h53.539V492.5l208.15-37.422v-61.235V37.5L184.646,0z M222.938,263.129c-6.997,0-12.67-7.381-12.67-16.486c0-9.104,5.673-16.485,12.67-16.485s12.67,7.381,12.67,16.485C235.608,255.748,229.935,263.129,222.938,263.129z"/>
                     </g>
                 </svg>
-            </a>
-            <a href="../processos/logout.php" alt="Sair" title="Sair">
-                <!-- Ícone de sair -->
             </a>
         </nav>
     </header>
@@ -84,6 +74,7 @@ $denuncias_avaliacoes = $pdo->query("SELECT Denuncia.*, Avaliacao.id_avaliacao, 
                     <?php endforeach; ?>
                 </tbody>
             </table>
+
             <h2>Avaliações Denunciadas</h2>
             <table>
                 <thead>
@@ -107,9 +98,7 @@ $denuncias_avaliacoes = $pdo->query("SELECT Denuncia.*, Avaliacao.id_avaliacao, 
                         <td><?= htmlspecialchars($denuncia['denunciante_nome']); ?></td>
                         <td><?= htmlspecialchars($denuncia['data_denuncia']); ?></td>
                         <td>
-                            <!-- Link para excluir a denúncia -->
                             <a href="../processos/excluir_denuncia.php?id=<?= $denuncia['id_denuncia']; ?>" class="editar-btn" onclick="return confirm('Tem certeza que deseja excluir esta denúncia?')">Excluir Denúncia</a>
-                            <!-- Link para excluir a avaliação -->
                             <a href="../processos/excluir_avaliacao.php?id=<?= $denuncia['id_avaliacao']; ?>" class="excluir-btn" onclick="return confirm('Tem certeza que deseja excluir esta avaliação?')">Excluir Avaliação</a>
                         </td>
                     </tr>
@@ -120,4 +109,3 @@ $denuncias_avaliacoes = $pdo->query("SELECT Denuncia.*, Avaliacao.id_avaliacao, 
     </main>
 </body>
 </html>
-
